d5f37a32c48060a5e78b606846f4ae217385f2a1
Tim Scarborough <scarborough.tim@gmail.com>
2021-11-27 23:47:01 -0500

    Updated password strength UI

--- src/components/PasswordStrength.jsx
+++ src/components/PasswordStrength.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react';
 import zxcvbn from 'zxcvbn';
 import PropTypes from 'prop-types';
-import { Typography } from '@mui/material';
+import { Typography, Chip } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    paddingRight: theme.spacing(2),
-    fontWeight: 'bold',
+    marginRight: theme.spacing(2),
   },
 }));
 
 function PasswordStrength({ password }) {
   const classes = useStyles();
   const [title, setTitle] = useState('');
+  const [chipColor, setChipColor] = useState('default');
   const [message, setMessage] = useState('');
 
   useEffect(() => {
@@ -24,18 +24,23 @@ function PasswordStrength({ password }) {
         case 0:
         case 1:
           setTitle('Poor');
+          setChipColor('error');
           break;
         case 2:
           setTitle('Weak');
+          setChipColor('warning');
           break;
         case 3:
           setTitle('OK');
+          setChipColor('info');
           break;
         case 4:
           setTitle('Strong');
+          setChipColor('success');
           break;
         default:
           setTitle('');
+          setChipColor('default');
           break;
       }
 
@@ -52,10 +57,12 @@ function PasswordStrength({ password }) {
 
   return (
     <>
-      <Typography variant="caption" display="inline" className={classes.title}>
-        {title}
-      </Typography>
-      <Typography variant="caption">{message}</Typography>
+      {password.length > 0 && (
+        <>
+          <Chip label={title} color={chipColor} className={classes.title} />
+          <Typography variant="caption">{message}</Typography>
+        </>
+      )}
     </>
   );
 }
